1e447f072a00be313e1484345899e7aabe9a0621
David <35436218+GitSoks@users.noreply.github.com>
2024-03-16 18:25:26 +0100

    Update docker-compose.yaml and settings.py

--- mqtt_led_controller_ui/settings.py
+++ mqtt_led_controller_ui/settings.py
@@ -1,8 +1,10 @@
-broker_address = "127.0.0.1"
-broker_port = 1883
-broker_username = "ui"
-broker_password = "password"
+broker_address = "mqtt_broker"  # the address of the MQTT broker (e.g. "mqtt_broker" for Docker, "localhost" for local)
+broker_port = 1883  # the port of the MQTT broker (default: 1883)
+broker_username = "ui"  # the username for the MQTT broker
+broker_password = "password"  # the password for the MQTT broker
 
-test_devices = False
+test_devices = False  # if True, the UI will add test devices to the list of devices
 
-led_ring_12_image_path = "./media/led_ring.png"
+led_ring_12_image_path = (
+    "./media/led_ring.png"  # the path to the image of the 12-LED ring
+)
