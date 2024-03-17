3efbe00c758ce59efcd7f3b747f913ddfd8cf673
David <35436218+GitSoks@users.noreply.github.com>
2024-03-17 10:59:04 +0100

    Update settings.py and media files

--- mqtt_led_controller_ui/settings.py
+++ mqtt_led_controller_ui/settings.py
@@ -3,7 +3,7 @@ broker_port = 1883  # the port of the MQTT broker (default: 1883)
 broker_username = "ui"  # the username for the MQTT broker
 broker_password = "password"  # the password for the MQTT broker
 
-test_devices = False  # if True, the UI will add test devices to the list of devices
+test_devices = True  # if True, the UI will add test devices to the list of devices
 
 led_ring_12_image_path = (
     "./media/led_ring.png"  # the path to the image of the 12-LED ring
